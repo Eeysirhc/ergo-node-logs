7f4da29cd48d96894ba2038587adcfcb04dfebd3
Eeysirhc <cyee428@gmail.com>
2023-08-01 09:49:33 -0700

    fix grep command

--- logs-process.sh
+++ logs-process.sh
@@ -5,7 +5,7 @@ gunzip *.log.gz
 
 # process headers
 # extract text | grab datetime & height | add log filename > save txt file
-grep "applied to UtxoState at height" *.log | awk '{print $1, $20}' | sed 's/.log:/\ /g' > $NODE-processed.txt
+grep "New height" *.log | awk '{print $1, $15}' | sed 's/.log:/\ /g' > $NODE-processed.txt
 
 # remove raw files
 rm *.log
